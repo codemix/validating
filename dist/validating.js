6913aaa1d59024ea40492fa21b7867e741eec4ce
Charles Pick <charles.pick@gmail.com>
2014-06-07 23:25:02 +0100

    update dependencies

--- dist/validating.js
+++ dist/validating.js
@@ -1,6 +1,6 @@
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.Validating=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
-var OBLIGATIONS = _dereq_('obligations');
 'use strict';
+var OBLIGATIONS = _dereq_('obligations');
 /**
  * The validator base class.
  * @type {Function}
@@ -125,8 +125,8 @@ for (i = 0; i < total; i++) {
     exports.define(name, validators[name]);
 }
 },{"./validator":2,"./validators":3,"obligations":5}],2:[function(_dereq_,module,exports){
-var OBLIGATIONS = _dereq_('obligations');
 'use strict';
+var OBLIGATIONS = _dereq_('obligations');
 var Class = _dereq_('classing');
 /**
  * Base class for Validators.
@@ -206,8 +206,8 @@ module.exports = Class.create({
     }
 });
 },{"classing":4,"obligations":5}],3:[function(_dereq_,module,exports){
-var OBLIGATIONS = _dereq_('obligations');
 'use strict';
+var OBLIGATIONS = _dereq_('obligations');
 /**
  * # Required Validator
  *
